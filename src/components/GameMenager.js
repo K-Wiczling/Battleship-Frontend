b4201ed982e921262f5e0ee00658715d10c578dc
K-Wiczling <wiczling.karol@gmail.com>
2022-08-26 15:18:53 +0200

    initialize 3

---
+++ src/components/GameMenager.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class  GameMenager extends Component{
+    render(){
+
+      return (
+      <>
+        <div className="game-menager">
+            <p>
+              GameMenager
+            </p>
+        </div>
+      </>
+    );
+  }
+}
+
+export default GameMenager;
