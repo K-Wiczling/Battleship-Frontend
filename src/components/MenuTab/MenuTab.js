f13c0b81d775ebb73051d7ab5cd25bf183d797ed
K-Wiczling <wiczling.karol@gmail.com>
2022-09-17 19:45:29 +0200

    create component MenuTab

---
+++ src/components/MenuTab/MenuTab.js
@@ -0,0 +1,16 @@
+//css
+import "./MenuTab.css"
+
+//React
+import React from 'react';
+
+//Single tab for use iside the menu
+const  MenuTab = (props) => {
+  
+    return (
+        <div className="ship-display">
+            
+        </div>
+    );
+  }
+export default MenuTab;
