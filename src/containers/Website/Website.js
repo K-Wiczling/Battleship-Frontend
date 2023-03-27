fd6348b74b2f729e123114d72787bc1068f0732f
kwiczling <wiczling.karol@gmail.com>
2023-03-27 19:44:19 +0200

    create website component folder

---
+++ src/containers/Website/Website.js
@@ -0,0 +1,33 @@
+//css
+import './App.css';
+
+//React stuff
+import React, { Component } from 'react';
+
+//Components
+
+//Containers
+
+//Redux
+// import { connect } from "react-redux"
+// import { changeModes } from "./actions"
+// import { MODE_GAME, MODE_MAIN_MENU, MODE_SPLASH_SCREEN } from './constants';
+
+const mapStateToProps = (state) => {
+  return {
+   
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    
+  }
+}
+//Main entrance to the app 
+class  Website extends Component {
+  render () {
+    
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Website);
---
+++ src/containers/Website/reducer.js
@@ -0,0 +1,55 @@
+import server from "../../classes/server";
+import { 
+    CHANGE_MODE, 
+    MODE_GAME, 
+    MODE_MAIN_MENU, 
+    MODE_SPLASH_SCREEN, 
+    SETUP_GAME_AI_DIFFICULTY, 
+    SETUP_GAME_BOARD_SIZE, 
+    SETUP_GAME_ENEMY_TYPE, 
+    SETUP_GAME_FLEET_TYPE, 
+    SETUP_GAME_FULL, 
+    SETUP_GAME_RANDOM
+} from "./constants";
+
+const  initialModeState = {
+    mode: MODE_GAME,
+}
+//Change game state
+export const changeMode = (state=initialModeState, action={}) => {
+    switch (action.type) {
+        case CHANGE_MODE:
+            return Object.assign({}, state, { mode: action.payload })
+        default:
+            return state
+    }
+}
+
+//Information for the GameMenager to start game
+const gameSetupState = {
+    enemyType: server.Params.enemyTpes.AI,
+    aiDifficulty: server.Params.aiDifficulty.easy,
+    fleetType: server.Params.fleetType.clasic,
+    boardSize: server.Params.boardSize.clasic,
+    randomSetupShips: false
+}
+export const setupGame = (state=gameSetupState, action={}) => {
+    switch (action.type) {
+        case SETUP_GAME_FULL:
+            return Object.assign({}, state, { 
+                enemyType: action.payload.enemyTpes,
+                aiDifficulty: action.payload.aiDifficulty,
+                fleetType: action.payload.fleetType,
+                boardSize: action.payload.boardSize
+             });
+             
+            case SETUP_GAME_ENEMY_TYPE: return Object.assign({}, state, { enemyType: action.payload});
+            case SETUP_GAME_AI_DIFFICULTY: return Object.assign({}, state, { aiDifficulty: action.payload});
+            case SETUP_GAME_FLEET_TYPE: return Object.assign({}, state, { fleetType: action.payload});
+            case SETUP_GAME_BOARD_SIZE: return Object.assign({}, state, { boardSize: action.payload});
+            case SETUP_GAME_RANDOM: return Object.assign({}, state, { randomSetupShips: action.payload});
+           
+        default:
+            return state;
+    }
+}
