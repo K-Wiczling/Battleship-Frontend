40b57d426739ab5489f11e7f77f5120559d2fd9b
kwiczling <wiczling.karol@gmail.com>
2023-03-27 21:17:01 +0200

    setup website redux structure

--- src/containers/Website/actions.js
+++ src/containers/Website/actions.js
@@ -1,1 +1,7 @@
-import { } from "./constants";
+import { SET_INTERACTION } from "./constants";
+
+//Change game state
+export const websiteInteraction = (interaction) => ({
+    type: SET_INTERACTION,
+    payload: interaction
+});
--- src/containers/Website/reducer.js
+++ src/containers/Website/reducer.js
@@ -1,1 +1,14 @@
-import { } from "./constants";
+import { LOGIN_INTERACTION, REGISTER_INTERACTION, SET_INTERACTION } from "./constants";
+
+const initialInteraction = {
+    interacton: none,
+}
+//Change game state
+export const setInteraction = (state = initialInteraction, action = {}) => {
+    switch (action.type) {
+        case SET_INTERACTION:
+            return Object.assign({}, state, { interacton: action.payload })
+        default:
+            return state
+    }
+}
