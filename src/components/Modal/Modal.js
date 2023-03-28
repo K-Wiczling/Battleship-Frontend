c3a8df360636103e168ed81425914cb53e14ac1c
kwiczling <wiczling.karol@gmail.com>
2023-03-28 19:38:58 +0200

    add modal component & change gitignore to not include local folder

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,19 @@
+//css
+import "./Head.css"
+
+//React
+import React from 'react';
+
+//Header component
+const  Head = ({ text, top }) => {
+    if(Number.isInteger(top)) top = 0;
+    return (
+        <>
+            <div className="head" style={{top: `${top}vh`}}>
+                <h2><a href="index.html">{ text }</a></h2>
+            </div>
+            <div className="spacer-top"></div>
+        </>
+    );
+  }
+export default Head;
