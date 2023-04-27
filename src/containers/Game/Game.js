8ced7398f4a5daf5942d1ae1daf437d66d3ebe71
kwiczling <wiczling.karol@gmail.com>
2023-04-27 19:16:52 +0200

    change some styles

--- src/containers/Game/Game.js
+++ src/containers/Game/Game.js
@@ -47,7 +47,7 @@ class Game extends Component {
         return (
             <>
                 <Head />
-                <div className="Game">
+                <div className="game">
                     <InGameMenu
                         menuVisibility={this.props.menuVisibility}
                     />
