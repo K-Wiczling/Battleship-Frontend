51ff3e44c5b7ea2348579a2e42806aefb295cd4f
kwiczling <wiczling.karol@gmail.com>
2023-04-27 20:55:19 +0200

    add test to validate eamil

--- __tests__/utility.test.js
+++ __tests__/utility.test.js
@@ -1,10 +1,10 @@
-import Validate from "../src/classes/validate";
+import { Validate } from "../src/classes/validate";
 
 describe("Check if email is in the correct form", () => {
     test("return true", () => {
-      expect(Validate.validateEmail('test@mail.com')).toEqual(output);
+      expect(Validate.validateEmail('test@mail.com')).toEqual(true);
     });
     test("return false", () => {
-      expect(Validate.validateEmail('wrong input')).toEqual([]);
+      expect(Validate.validateEmail('wrong input')).toEqual(false);
     });
   }); 
